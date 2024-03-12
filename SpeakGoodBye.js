6913a7c9ab9b006d9341d2cef9beb9f5e7aa345a
CryCatCC <160016979+CryCatCC@users.noreply.github.com>
2024-03-13 00:16:10 +0200

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,24 +1,9 @@
-// let speakWord = "Good Bye";
-
-
-// function speak(name) {
-//   console.log(speakWord + " " + name);
-// }
-
-
-// let speakWordGoodbye = "Good Bye";
-
-// function speakGoodbye(name) {
-//   console.log(speakWordGoodbye + " " + name);
-// }
-
-
 (function (window) {
   let personGoodBye = {};
-  personGoodBye.speakWordGoodbye = "Good Bye";
+  let speakWordGoodbye = "Good Bye";
 
   personGoodBye.speakGoodbye = function (name) {
-    console.log(personGoodBye.speakWordGoodbye + " " + name);
+    console.log(speakWordGoodbye + " " + name);
   }
 
   window.personGoodBye = personGoodBye;
